41767017d80e336f3b19f5027857e830eae2c0e5
aksel5741 <aksel5741@gmail.com>
2020-02-11 13:35:24 +0200

    final 2.0

--- resources/views/Comments/comment.blade.php
+++ resources/views/Comments/comment.blade.php
@@ -1,5 +1,5 @@
 @foreach($post->comment as $comment)
-    <div style="border: 1px solid red">
+    <div style="border: 1px solid gray">
         <a href="{{$comment->user==null ? '': route('profile',['user'=>$post->user->id])}}"><img class="[ img-circle pull-left ]" width="50px" height="50px" src="/storage/avatars/{{$comment->user==null ? 'user.png' : $comment->user->avatar}}" alt="Mouse0270" />
         <h3>{{$comment->user==null ? 'Guest' : $comment->user->name}}</h3></a>
         {{$comment->comm_content}}
--- routes/web.php
+++ routes/web.php
@@ -24,19 +24,19 @@ Route::get('filtered-posts','CategoryController@make');
 Route::get('categories-filter', 'CategoryController@filter')->name('cat_filter');
 
 
-Route::get('posts-create', 'PostController@index')->name('post-create');
+Route::get('posts-create', 'PostController@index')->name('post-create')->middleware('verified');;
 Route::post('posts-create', 'PostController@create');
 
 Route::post('create-category', 'CategoryController@create')->name('category');
 
-Route::get('posts-change/{post}', 'PostController@postForChange')->name('post-change');
-Route::patch('posts-change/{post}', 'PostController@postChange')->name('patch-post');
+Route::get('posts-change/{post}', 'PostController@postForChange')->name('post-change')->middleware('verified');;
+Route::patch('posts-change/{post}', 'PostController@postChange')->name('patch-post')->middleware('verified');;
 
 Route::get('post/{post}', 'PostController@post')->name('post-page');
 Route::post('post/{post}', 'CommentsController@create')->name('post');
 
 Route::get('profile/{user}', 'UserController@profile')->name('profile');
-Route::post('profile', 'UserController@updateAvatar');
+Route::post('profile', 'UserController@updateAvatar')->middleware('verified');;
 
 Route::get('all-posts','PostController@show')->name('all-posts');
 Route::get('my-posts', 'PostController@usersPost')->name('my-posts');
